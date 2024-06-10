1cb54d59c242356b6a21687534d62a0d0b844482
k1mjunyoung <me@kimjunyoung.com>
2024-06-10 16:16:24 +0900

    Initial commit

---
+++ src/main/java/SpringAuthApiBoilerplate/SpringAuthApiBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package SpringAuthApiBoilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthApiBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthApiBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringAuthApiBoilerplate/SpringAuthApiBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringAuthApiBoilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthApiBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
